58e9b44241d5e191513951e33a5bc786c5ea678e
stachlew <stachlewski.m@gmail.com>
2017-04-22 01:05:05 +0200

    Działająca rejestracja uczestnika.

--- ng2/src/app/event-view/register-participant/register-participant.component.ts
+++ ng2/src/app/event-view/register-participant/register-participant.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ParticipantForm} from "../../_model/domainClass";
+import {HttpSecService} from "../../_service/util/http-sec.service";
+import {Http,Response} from "@angular/http";
 
 @Component({
   selector: 'app-register-participant',
@@ -10,10 +13,26 @@ export class RegisterParticipantComponent{
   public visible = false;
   private visibleAnimate = false;
 
-  constructor(){}
+  private participant:ParticipantForm;
+  private firstLoad: boolean;
+  private loading: boolean;
+  private successRegister: boolean;
+
+
+  constructor(private http: Http, private myHttp: HttpSecService){
+    this.participant = new ParticipantForm;
+    this.firstLoad=true;
+    this.loading=false;
+    this.successRegister=false;
+  }
 
   public show(): void {
+    console.info("first: "+ this.firstLoad + " loadin: "+ this.loading + " success: "+ this.successRegister );
+    this.participant= new ParticipantForm;
     this.visible = true;
+    this.firstLoad = true;
+    this.loading = false;
+    this.successRegister = false;
     setTimeout(() => this.visibleAnimate = true, 100);
   }
 
@@ -21,6 +40,23 @@ export class RegisterParticipantComponent{
     this.visibleAnimate = false;
     setTimeout(() => this.visible = false, 300);
   }
+
+  public save(idEvent:number): void {
+    this.firstLoad=false;
+    this.loading=true;
+    this.participant.idEvent=idEvent;
+    console.info("ZAPISUJE: "+ this.participant.idEvent + " "+ this.participant.firstname + " " + this.participant.lastname + " "+ this.participant.email);
+
+    this.firstLoad=false;
+    this.http.post(this.myHttp.getUrl()+'/api/event/view/register/postNewParticipant',this.participant,this.myHttp.postConfig())
+      .subscribe((data: Response)=> {this.successRegister = data.json(),this.endRequest()});
+  }
+
+  private endRequest(){
+    this.loading=false;
+  }
+
+
 
   public onContainerClicked(event: MouseEvent): void {
     if ((<HTMLElement>event.target).classList.contains('modal')) {
--- src/main/java/pl/wat/api/event/view/register/EventRegisterController.java
+++ src/main/java/pl/wat/api/event/view/register/EventRegisterController.java
@@ -1,9 +1,27 @@
 package pl.wat.api.event.view.register;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.wat.logic.event._model.ParticipantForm;
+import pl.wat.logic.event.view.EventRegisterService;
 
 @RestController
 @RequestMapping("/api/event/view/register")
 public class EventRegisterController {
+
+    @Autowired
+    EventRegisterService eventRegisterService;
+
+
+    @RequestMapping(value = "/postNewParticipant",method = RequestMethod.POST)
+    public boolean registerParticipant(@RequestBody ParticipantForm participantForm){
+        if(participantForm!=null){
+            return eventRegisterService.registeredParticipant(participantForm);
+        }
+        return false;
+    }
+
 }
