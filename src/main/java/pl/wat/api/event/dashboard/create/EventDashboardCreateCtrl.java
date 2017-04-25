8406c7b1031979a3707dfc5f818a4ddb6d73662d
Przemohawryl <przemohawryl@gmail.com>
2017-04-25 16:43:09 +0200

    Added DictionaryService

--- src/main/java/pl/wat/api/event/dashboard/create/EventDashboardCreateCtrl.java
+++ src/main/java/pl/wat/api/event/dashboard/create/EventDashboardCreateCtrl.java
@@ -1,10 +1,17 @@
 package pl.wat.api.event.dashboard.create;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.wat.logic.util.DictionaryService;
 
 @RestController
 @RequestMapping("/api/event/dashboard/create")
 public class EventDashboardCreateCtrl {
+
+    @Autowired
+    private DictionaryService dictionaryService;
+
+
 }
--- src/main/java/pl/wat/logic/event/_model/EventCreateForm.java
+++ src/main/java/pl/wat/logic/event/_model/EventCreateForm.java
@@ -7,18 +7,20 @@ import java.sql.Timestamp;
  */
 public class EventCreateForm {
 
-    private int idEvent;
     private String title;
+    private String description;
     private Timestamp startTime;
     private Timestamp endTime;
 
-    public int getIdEvent() {
-        return idEvent;
-    }
+    private int capacity;
+    private boolean freeEntrance;
 
-    public void setIdEvent(int idEvent) {
-        this.idEvent = idEvent;
-    }
+    private String regionName;
+    private String cityName;
+    private String streetName;
+    private String streetNo;
+    private String geoLength;
+    private String geoWidth;
 
     public String getTitle() {
         return title;
@@ -26,6 +28,14 @@ public class EventCreateForm {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Timestamp getStartTime() {
@@ -44,6 +54,54 @@ public class EventCreateForm {
         this.endTime = endTime;
     }
 
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public boolean isFreeEntrance() {
+        return freeEntrance;
+    }
+
+    public void setFreeEntrance(boolean freeEntrance) {
+        this.freeEntrance = freeEntrance;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getStreetNo() {
+        return streetNo;
+    }
+
+    public void setStreetNo(String streetNo) {
+        this.streetNo = streetNo;
+    }
+
     public String getGeoLength() {
         return geoLength;
     }
@@ -59,7 +117,4 @@ public class EventCreateForm {
     public void setGeoWidth(String geoWidth) {
         this.geoWidth = geoWidth;
     }
-
-    private String geoLength;
-    private String geoWidth;
 }
