0ca506f45b82aa35c36f6b43012e5d4450209f30
Przemohawryl <przemohawryl@gmail.com>
2017-04-25 15:49:57 +0200

    Indirect EventCreateForm model

--- src/main/java/pl/wat/logic/event/_model/EventCreateForm.java
+++ src/main/java/pl/wat/logic/event/_model/EventCreateForm.java
@@ -1,7 +1,65 @@
 package pl.wat.logic.event._model;
 
+import java.sql.Timestamp;
+
 /**
  * Created by michal on 19.04.2017.
  */
 public class EventCreateForm {
+
+    private int idEvent;
+    private String title;
+    private Timestamp startTime;
+    private Timestamp endTime;
+
+    public int getIdEvent() {
+        return idEvent;
+    }
+
+    public void setIdEvent(int idEvent) {
+        this.idEvent = idEvent;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getGeoLength() {
+        return geoLength;
+    }
+
+    public void setGeoLength(String geoLength) {
+        this.geoLength = geoLength;
+    }
+
+    public String getGeoWidth() {
+        return geoWidth;
+    }
+
+    public void setGeoWidth(String geoWidth) {
+        this.geoWidth = geoWidth;
+    }
+
+    private String geoLength;
+    private String geoWidth;
 }
