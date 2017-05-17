f5bfb304a7697964c7d5331e28e2cea69ac768b6
Tomasz Hodyra <t.hodyra@gmail.com>
2017-05-17 11:43:07 +0200

    Add functionality (delete user).

--- src/main/java/pl/wat/api/administration/users/AdministrationUsersController.java
+++ src/main/java/pl/wat/api/administration/users/AdministrationUsersController.java
@@ -2,18 +2,11 @@ package pl.wat.api.administration.users;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.wat.db.domain.user.User;
 import pl.wat.logic.administration.users.AdministrationUsersService;
 import pl.wat.logic.user._model.UserAdministrationSearchForm;
 import pl.wat.logic.user._model.UserAdministrationSearchResult;
-import pl.wat.logic.user._model.UserRegisterForm;
 import pl.wat.logic.user.account.UserAccountService;
 import pl.wat.logic.user.search.UserSearchService;
 
@@ -29,18 +22,6 @@ public class AdministrationUsersController {
   private UserSearchService userSearchService;
   @Autowired
   private UserAccountService userAccountService;
-
-  @GetMapping("/getAllUsers")
-  @ResponseBody
-  public List<User> getAllUsers() {
-    return administrationUsersService.getAllUsers();
-  }
-
-  @GetMapping("/getUserByEmail/{email}")
-  @ResponseBody
-  public User getUserByEmail(@PathVariable String email) {
-    return administrationUsersService.getUserByEmail(email);
-  }
 
   @GetMapping("/getUser/{id}")
   @ResponseBody
